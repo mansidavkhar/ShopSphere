63ff39eb02fc203a5e3ea3e31eb2b537d6117b34
manishraje28 <manishbraje@gmail.com>
2024-12-28 22:25:29 +0530

    added ul

--- frontend/src/Components/Navbar/Navbar.jsx
+++ frontend/src/Components/Navbar/Navbar.jsx
@@ -10,7 +10,12 @@ const Navbar = () => {
             <img src={logo} alt="" />
             <p>ShopSphere</p>
         </div>
-
+        <ul className="nav-menu">
+            <li>Shop <hr /></li>
+            <li>Men</li>
+            <li>Women</li>
+            <li>Kids</li>
+        </ul>
        
 
         <div className="nav-login-cart">
